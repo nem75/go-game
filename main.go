fb6d54bd1bffdf005926f50d0bd3a609a36565c3
Jens Bremmekamp <jens.bremmekamp@chefkoch.de>
2020-08-31 11:27:39 +0200

    Run go fmt

--- main.go
+++ main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-        "log"
+	"log"
 
-        "github.com/hajimehoshi/ebiten"
-        "github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 var (
-        err         error
-        background  *ebiten.Image
+	err        error
+	background *ebiten.Image
 )
 
 func init() {
-        background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
-        if err != nil {
-                log.Fatal(err)
-        }
+	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func update(screen *ebiten.Image) error {
-        if ebiten.IsDrawingSkipped() {
-                return nil
-        }
-        op := &ebiten.DrawImageOptions{}
-        op.GeoM.Translate(0,0)
-        screen.DrawImage(background, op)
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(0, 0)
+	screen.DrawImage(background, op)
 
-        return nil
+	return nil
 }
 
 func main() {
-        if err := ebiten.Run(update, 320, 240, 2, "Ebiten Hello World"); err != nil {
-                log.Fatal(err)
-        }
+	if err := ebiten.Run(update, 320, 240, 2, "Ebiten Hello World"); err != nil {
+		log.Fatal(err)
+	}
 }
